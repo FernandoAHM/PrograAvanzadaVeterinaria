529767c16069701ae5dc45d0f94ff6d41aa983a9
seba-monsalve <sebastian.monsalve@usach.cl>
2019-10-15 22:47:57 -0300

    Avance: Todo conectado. Faltan clases y controladores

---
+++ source/Derivacion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cliente.source
+{
+    public class Derivacion
+    {
+        private int id;
+        private String nombre_vet;
+        private String nombre_cliente;
+
+    }
+}
